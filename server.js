96982d5f98025713dc5d4e4d748d5119cbddaca8
chrizgx <102369753+chrizgx@users.noreply.github.com>
2023-07-28 17:16:42 +0300

    1 file modified

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Install Express Server for Heroku Deployment
 const express = require('express');
-cosnt shrinkRay = require('shrink-ray');
+const shrinkRay = require('shrink-ray');
 
 const app = express();
 // Serve static files from dist directory
@@ -8,9 +8,9 @@ app.use(shrinkRay());
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html');
-})
+  res.sendFile(__dirname + '/dist/index.html');
+});
 
-app.listen(process.env.PORT || 8080, function() {
-    console.log('Serving Angular SPA from "dist" folder.')
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Serving Angular SPA from "dist" folder.');
 });
