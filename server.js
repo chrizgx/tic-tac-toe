e55d8e1cefc9cb02e9dde47db024dc9788f9de58
chrizgx <102369753+chrizgx@users.noreply.github.com>
2023-07-28 16:06:03 +0300

    3 files modified

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Install Express Server for Heroku Deployment
 const express = require('express');
+cosnt shrinkRay = require('shrink-ray');
 
 const app = express();
 // Serve static files from dist directory
-
+app.use(shrinkRay());
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function (req, res) {
